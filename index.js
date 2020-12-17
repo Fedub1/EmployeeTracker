7f4c7735fdc56e835c25e339f3c9572e428011a6
Felicia Wright <fedub1@yahoo.com>
2020-12-16 20:20:52 -0500

    updated index

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-var mysql = require('mysql');
-var inquirer = require('inquirer');
+const mysql = require('mysql')
+const inquirer = require('inquirer')
+const cTable = require('console.table');
+
 
 //Create Connection
 var connection = mysql.createConnection({
@@ -15,15 +17,16 @@ var connection = mysql.createConnection({
 	password: "root",
 	database: "employee_db"
 });
+
 //Connect to MySql
-connection.connect(function (err) {
+connection.connect(function(err) {
 	if (err) throw err;
+				
 	// console.log("connected as id " + connection.threadId + "\n");
 	runSearch();
 });
-// const app = inquirer();
-
-function runSearch() {
+	
+ function runSearch() {
 	inquirer
 	.prompt({
 		name: "action",
@@ -33,18 +36,10 @@ function runSearch() {
 			"Query all employees",
 			"Query all departments",
 			"Query all roles",
-			// "Add employee",
-			// "Add department",
-			// "Add role",
-			// "Delete employee",
-			// "Delete department",
-			// "Delete role",
-			// "Update role"
-			
 			// "exit"
 		]
 	})
-		.then(function (answer) {
+		.then(function(answer) {
 			switch (answer.action) {
 				case "View all employees":
 					employeeSearch();
@@ -58,66 +53,33 @@ function runSearch() {
 					roleSearch();
 					break;
 
-				// case "Add employee":
-				// 	addEmployee();
-				// 	break;
-					  
-				// case "Add department":
-				// 	addDepartment();
+				// case "exit":
+				// 	connection.end();
 				// 	break;
 
-				// case "Add role":
-				// 	addRole();
-				// 	break;
-
-				// case "Delete employee":
-				// 	deleteEmployee();
-				// 	break;
-
-				// case "Delete department":
-				// 	deleteDepartment();
-				// 	break;
-					
-				// case "Delete role":
-				// 	deleteRole();
-				// 	break;
-
-				// case "Update Role":
-				// 	updateRole();
-				// 	break;
-
-				
-		// 		case "exit":
-        // connection.end();
-        // break;
-			}
-		});
-	}
+function employeeSearch(){
+	console.log("Selecting all employee...\n");
+	connection.query("SELECT * FROM employee", function(err, data) { 
+	
+	// if (err) throw err;
+	cTable(data);
+	departmentSearch();
+	})
+}
+function departmentSearch(){
+		console.log("Selecting all department...\n");
+		connection.query("SELECT * FROM department", function(err, data) { 
 		
-
-function employeeSearch() {
-	// console.log("Selecting all employees...\n");
-	connection.query("SELECT * FROM employee", function(err, data) {
-		if (err) throw err;
-	  console.table(data);
-	  runSearch();
-	});
-
-};
-  function departmentSearch() {
-	// console.log("Selecting all department...\n");
-	connection.query("SELECT * FROM department", function(err, data) {
-		if (err) throw err;
-	  console.table(data);
-	  runSearch();
-	});
-};
- 
-  function roleSearch() {
-	// console.log("Selecting all roles...\n");
-	connection.query("SELECT * FROM role", function(err, data) {
-		if (err) throw err;
-	  console.table(data);
-	  runSearch;
-	  connection.end();
-	});}
+		// if (err) throw err;
+		cTable(data);
+		roleSearch();
+		})
+	}
+function roleSearch(){
+			console.log("Selecting all role...\n");
+			connection.query("SELECT * FROM role", function(err, data) { 
+			// if (err) throw err;
+			cTable(data);
+			  
+			})
+}};},)};
