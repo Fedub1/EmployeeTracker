5e6a2708329c0af49766c4b9af87fbc3163beb0e
Felicia Wright <fedub1@yahoo.com>
2020-12-17 01:48:54 -0500

    updated index

--- index.js
+++ index.js
@@ -33,9 +33,17 @@ connection.connect(function(err) {
 		type: "list",
 		message: "What would you like to do?",
 		choices: [
-			"Query all departments",
-			"Query all roles",
-			"Query all employees",
+			"View all departments",
+			"View all roles",
+			"View all employees",
+			"Add department",
+			"Add role",
+			"Add employee",
+			"Update employee",
+			"Delete Employee",
+
+
+
 			"exit"
 		]
 	})
@@ -53,35 +61,179 @@ connection.connect(function(err) {
 					employeeSearch();
 					break;
 
+				case "Add department":
+					addDepartment();
+					break;
+					  
+				case "Add role":
+					createRole();
+					break;
+
+				case "Add employee":
+					createEmployee();
+					break;
+				case "Update employee":
+						updateEmployee();
+						break;
+				case "Delete employee":
+							deleteEmployee();
+							break;		
 				// case "exit":
 				// 	connection.end();
 				// 	break;
 
 function departmentSearch(){
-	console.log("Selecting all department...\n");
+	console.log("Selecting all departments...\n");
 	connection.query("SELECT * FROM department", function(err, data) { 
 	
 	if (err) throw err;
-	cTable(data);
+	console.table(data);
 	roleSearch();
 	})
 }
 function roleSearch(){
-		console.log("Selecting all role...\n");
-		connection.query("SELECT * FROM role", function(err, data) { 
+	console.log("Selecting all roles...\n");
+	connection.query("SELECT * FROM role", function(err, data) { 
 		
-		if (err) throw err;
-		cTable(data);
-		employeeSearch();
-		})
+	if (err) throw err;
+	console.table(data);
+	employeeSearch();
+	})
 	}
 function employeeSearch(){
-			console.log("Selecting all employees...\n");
-			connection.query("SELECT * FROM employee", function(err, data) { 
-			if (err) throw err;
-			cTable(data);
-			runSearch();
-			})
-}};
-},
-)};
+	console.log("Selecting all employees...\n");
+	connection.query("SELECT * FROM employee", function(err, data) { 
+	if (err) throw err;
+	console.table(data);
+	addDepartment();
+			}),
+		
+	
+function addDepartment() {
+	
+	inquirer
+	.prompt([
+		{
+			name: "department",
+			type: "input",
+			message: "Enter new department"
+		}])
+
+	.then(function(answer) {	
+	connection.query(
+		"INSERT INTO department (name) VALUES (?)", 
+		[answer.department],
+	function(err) {
+	if (err) throw err;
+	console.table("New Department added");
+// Call addRole AFTER the INSERT completes
+
+    addRole();
+	});},
+	// logs the actual query being run
+	function addRole() {
+		inquirer
+	.prompt([
+		{
+			name: "title",
+			type: "input",
+			message: "Enter new role",
+
+			name: "salary",
+			type: "number",
+			message: "Enter new salary",
+
+			name: "department_id",
+			type: "number",
+			message: "Enter new department id"
+		}])
+
+				.then(function(answer) {	
+				connection.query(
+					"INSERT INTO role (title, salary, department_id) VALUES (?,?,?)", 
+					[answer.title,
+					 answer.salary,
+					 answer.department_id],
+				function(err) {
+				if (err) throw err;
+				console.table("New Role added");
+	// Call addEmployee AFTER the INSERT completes
+				
+		addEmployee();
+		})})});
+		// logs the actual query 
+						
+		function addEmplyee() {
+			inquirer
+			.prompt([
+				{
+			name: "first_name",
+			type: "input",
+			message: "Enter employee first name",
+
+			name: "last_name",
+			type: "input",
+			message: "Enter employee last name",
+
+			name: "role_id",
+			type: "number",
+			message: "Enter employee role id",
+
+			name: "manager_id",
+			type: "manager",
+			message: "Enter employee manager id"
+			}])
+		}}
+			.then(function(answer) {	
+				connection.query("INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?,?,?,?)", res.role),
+			[answer.first_name,
+			answer.last_name,
+			answer.role_id,
+			answer.manager_id],
+			function(err) {
+				if (err) throw err;
+				console.table("New employee added");
+			updateEmployee();
+			}},
+			
+			// Call updateEmployee AFTER the INSERT completes
+			function updateEmployee() {
+				inquirer
+				.prompt([
+					{
+				message: "Enter first name of employee you want to update",		
+				name: "first_name",
+				type: "input",
+
+				message: "Enter employee new last name",
+				name: "last_name",
+				type: "input"
+					}])
+				.then(function(answer) {	
+					connection.query("UPDATE employee SET last_name WHERE first_name =?", 
+				[answer.last_name,
+				answer.first_name],
+				function(err) {
+					if (err) throw err;
+					console.table("New employee added");
+				deleteEmployee();
+	
+				function deleteEmployee() {
+					inquirer
+				.prompt([
+					{
+				message: "Enter first name of employee you want to delete",		
+				name: "first_name",
+				type: "input",
+					}])
+					.then(function(answer) {
+					  "DELETE FROM employee  WHERE first_name = ?",
+					  [answer.first_name],
+					  
+					  function(err) {
+						if (err) throw err;
+						console.table("Deleted Employee");
+						
+						connection.end();
+					  		
+					  };});}})})},)}}})}
